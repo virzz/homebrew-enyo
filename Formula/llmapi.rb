2750a2dc4c1566463d6f1fb258b6f3931e4a644b
陌竹 <mozhu233@outlook.com>
2026-09-23 18:58:37 +0800

    ✨ feat(homebrew): add llmapi formula

---
+++ Formula/llmapi.rb
@@ -0,0 +1,38 @@
+class Llmapi < Formula
+  desc "Multi-provider LLM API protocol conversion proxy"
+  homepage "https://github.com/virzz/llmapi-rs"
+  version "0.1.0"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  on_macos do
+    on_arm do
+      url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-darwin-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+
+    on_intel do
+      url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-darwin-amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    depends_on arch: :x86_64
+
+    url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-linux-amd64.tar.gz"
+    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  end
+
+  def install
+    bin.install "llmapi"
+  end
+
+  test do
+    assert_match "Start the HTTP server", shell_output("#{bin}/llmapi --help")
+  end
+end
