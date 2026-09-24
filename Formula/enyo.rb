20d4739db84f2aa2d4ee3f7f3ac95f3c8eef4ffc
陌竹 <mozhu233@outlook.com>
2026-09-24 13:10:55 +0800

    🐞 fix(homebrew): remove redundant formula versions

--- Formula/enyo.rb
+++ Formula/enyo.rb
@@ -1,7 +1,6 @@
 class Enyo < Formula
   desc "Cyber Swiss Army Knife for terminal"
   homepage "https://github.com/virzz/enyo-rs"
-  version "0.1.3"
   license "MIT"
 
   livecheck do
@@ -10,14 +9,14 @@ class Enyo < Formula
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/virzz/enyo-rs/releases/download/v#{version}/enyo-darwin-arm64.tar.gz"
+    url "https://github.com/virzz/enyo-rs/releases/download/v0.1.3/enyo-darwin-arm64.tar.gz"
     sha256 "1d4d2981950cc59e3b500681f484eeec339fdf920ae053111d5e0289ccd08c58"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/virzz/enyo-rs/releases/download/v#{version}/enyo-darwin-amd64.tar.gz"
+    url "https://github.com/virzz/enyo-rs/releases/download/v0.1.3/enyo-darwin-amd64.tar.gz"
     sha256 "eb416239aefd7118b2fa1cd191f6f52be18554a6f37f1bd901562419ab16b86b"
   elsif OS.linux?
     depends_on arch: :x86_64
-    url "https://github.com/virzz/enyo-rs/releases/download/v#{version}/enyo-linux-amd64.tar.gz"
+    url "https://github.com/virzz/enyo-rs/releases/download/v0.1.3/enyo-linux-amd64.tar.gz"
     sha256 "8172d4d198d76ef0506afff5a4b388d4e506d5f9b6643b649ec02fdbc24fa52f"
   end
 
--- Formula/llmapi.rb
+++ Formula/llmapi.rb
@@ -1,7 +1,6 @@
 class Llmapi < Formula
   desc "Multi-provider LLM API protocol conversion proxy"
   homepage "https://github.com/virzz/llmapi-rs"
-  version "0.1.0"
   license "MIT"
 
   livecheck do
@@ -10,14 +9,14 @@ class Llmapi < Formula
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-darwin-arm64.tar.gz"
+    url "https://github.com/virzz/llmapi-rs/releases/download/v0.1.0/llmapi-darwin-arm64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-darwin-amd64.tar.gz"
+    url "https://github.com/virzz/llmapi-rs/releases/download/v0.1.0/llmapi-darwin-amd64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   elsif OS.linux?
     depends_on arch: :x86_64
-    url "https://github.com/virzz/llmapi-rs/releases/download/v#{version}/llmapi-linux-amd64.tar.gz"
+    url "https://github.com/virzz/llmapi-rs/releases/download/v0.1.0/llmapi-linux-amd64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
 
