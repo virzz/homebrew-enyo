7a940b46a2d6259009ca14d7e627a74230667b19
陌竹 <mozhu233@outlook.com>
2026-09-24 12:50:21 +0800

    🐞 fix(homebrew): provide default Linux formula URLs

--- Formula/llmapi.rb
+++ Formula/llmapi.rb
@@ -1,6 +1,8 @@
 class Llmapi < Formula
   desc "Multi-provider LLM API protocol conversion proxy"
   homepage "https://github.com/virzz/llmapi-rs"
+  url "https://github.com/virzz/llmapi-rs/releases/download/v0.1.0/llmapi-linux-amd64.tar.gz"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   license "MIT"
 
   livecheck do
@@ -22,15 +24,6 @@ class Llmapi < Formula
 
   on_linux do
     depends_on arch: :x86_64
-
-    on_arm do
-      disable! "only x86_64 Linux binaries are published"
-    end
-
-    on_intel do
-      url "https://github.com/virzz/llmapi-rs/releases/download/v0.1.0/llmapi-linux-amd64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-    end
   end
 
   def install
